e0ee87030af7edca0d415863f71f6bc48aa2b964
tienDat203 <datntph26471@fpt.edu.vn>
2022-11-07 22:59:56 +0700

    Test

---
+++ app/src/main/java/com/example/DUAN/BaiThiActivity.java
@@ -0,0 +1,14 @@
+package com.example.DUAN;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaiThiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_thi);
+    }
+}
